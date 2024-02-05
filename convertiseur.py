27eeec7f1fbdac157531aa3541b8036552feebda
damienlevy <damienlevy546@gmail.com>
2024-02-05 22:56:32 +0100

    first commit

---
+++ convertiseur.py
@@ -0,0 +1,51 @@
+# import module
+from pdf2image import convert_from_path
+from PIL import Image
+
+def pdf_to_png(path:str):
+	# Store Pdf with convert_from_path function
+	try:
+		images = convert_from_path(path)
+	except:
+		print("erreur")
+	else:	
+		for index, image in enumerate(images):
+			image.save('page{0}.png'.format(index), 'PNG')
+		print("fin de traitement.")
+
+def jpg_to_png(path:str):
+	try:
+		img = Image.open(path)
+	except:
+		print("erreur.")
+	else:
+		path_sortie = '{0}.png'.format(path.split('.')[0])
+		img.save(path_sortie)
+
+def png_to_jpg(path:str):
+	try:
+		img = Image.open(path)
+	except:
+		print("erreur.")
+	else:
+		path_sortie = '{0}.jpg'.format(path.split('.')[0])
+		img.save(path_sortie)
+
+
+if __name__ == '__main__':
+	choix = """
+	0 : pdf_to_png
+	1 : jpg_to_png
+	2 : png_to_jpg
+	"""
+	choix = int(input(choix))
+	match choix:
+		case 0:
+			path = input("path du pdf ?")
+			pdf_to_png(path)
+		case 1:
+			path = input("path du jpg ?")
+			jpg_to_png(path)
+		case 2:
+			path = input("path du png ?")
+			png_to_jpg(path)
